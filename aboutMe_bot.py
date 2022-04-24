2e23e046fecbaa171e42a9aa2abdafe67679dc13
tsai8890 <asdpeter6520@gmail.com>
2022-04-25 05:09:29 +0800

    Add Comments & Create image_url.py
    1. Add comments into each part of aboutMe_bot.py.
    
    2. Create image_url.py, and use 'url'-prefixed variables
    to define the url of the sources of images, such as 'url_IMAGE'.

--- aboutMe_bot.py
+++ aboutMe_bot.py
@@ -1,3 +1,4 @@
+""" Import from flask & linebot """
 from flask import Blueprint, request, abort
 
 from linebot import LineBotApi, WebhookHandler
@@ -9,35 +10,36 @@ from linebot.models import (
     FollowEvent,
 
     TextSendMessage, 
-    TemplateSendMessage, 
     ImageSendMessage, 
-    StickerSendMessage,
-
-    ButtonsTemplate, 
-    PostbackTemplateAction
 )
+
 
 """ Self Defined Modules """
 from utils.template import (
     main_template_send,
     options_template_send
 )
+
 from utils.postback import *
+from utils.image_url import *
 from utils.config import linebot_config
+
 from utils.message_routine import (
     msg_IntroMyself,
     msg_NASA_Experience,
     msg_PA_Experience
 )
 
+
+""" Prepare the application, line_api, and webhook handler """
 aboutMe_bot = Blueprint('LINEBot', __name__)
 line_bot_api = LineBotApi(linebot_config['LINE_CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(linebot_config['LINE_CHANNEL_SECRET'])
 
+
+""" the webhook event receiver """
 @aboutMe_bot.route('/callback', methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
-    # && get request body as text
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
 
@@ -49,6 +51,8 @@ def callback():
 
     return 'OK'
 
+
+""" Follow Event Handler """
 @handler.add(FollowEvent)
 def handle_new_following(event):
     line_bot_api.reply_message (
@@ -61,6 +65,8 @@ def handle_new_following(event):
         ] 
     )
 
+
+""" Message Event Handler """
 @handler.add(MessageEvent)
 def handle_message(event):
     msg = event.message
@@ -69,6 +75,8 @@ def handle_message(event):
         options_template_send
     )
     
+
+""" Postback Event Handler """
 @handler.add(PostbackEvent)
 def handle_postback(event):
     data = event.postback.data
@@ -92,12 +100,12 @@ def handle_postback(event):
                     text = '這是我的履歷',
                 ),
                 ImageSendMessage (
-                    original_content_url = 'https://www.csie.ntu.edu.tw/~b08902016/resume.jpg',
-                    preview_image_url = 'https://www.csie.ntu.edu.tw/~b08902016/resume.jpg'
+                    original_content_url = url_RESUME,
+                    preview_image_url = url_RESUME
                 ),
                 options_template_send
             ]
-            )
+        )
 
     elif data == postback_NASA:
         line_bot_api.reply_message (
@@ -110,12 +118,12 @@ def handle_postback(event):
                     text = '這是我們DNS組的監控網站介面'
                 ),
                 ImageSendMessage (
-                    original_content_url = 'https://www.csie.ntu.edu.tw/~b08902016/DNS_monitor.png',
-                    preview_image_url = 'https://www.csie.ntu.edu.tw/~b08902016/DNS_monitor.png'
+                    original_content_url = url_DNS_MONITOR,
+                    preview_image_url = url_DNS_MONITOR
                 ),
                 options_template_send
             ]
-            )
+        )
     
     elif data == postback_PA:
         line_bot_api.reply_message (
@@ -128,10 +136,10 @@ def handle_postback(event):
                     text = '這是我去年十月擔任台大經濟系系卡的主控時，我的好朋友幫我拍的照片'
                 ),
                 ImageSendMessage (
-                    original_content_url = 'https://www.csie.ntu.edu.tw/~b08902016/PA.jpg',
-                    preview_image_url = 'https://www.csie.ntu.edu.tw/~b08902016/PA.jpg'
+                    original_content_url = url_PA_PHOTO,
+                    preview_image_url = url_PA_PHOTO
                 ),
                 options_template_send
             ]
-            )
+        )
     
---
+++ utils/image_url.py
@@ -0,0 +1,3 @@
+url_DNS_MONITOR = 'https://www.csie.ntu.edu.tw/~b08902016/DNS_monitor.png'
+url_RESUME = 'https://www.csie.ntu.edu.tw/~b08902016/resume.jpg'
+url_PA_PHOTO = 'https://www.csie.ntu.edu.tw/~b08902016/PA.jpg'
